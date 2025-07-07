9e3b701b63095b9c94c89179c76a12083681fedc
SomayaAhmed1430 <s.ramadan02185@dmu.edu.eg>
2025-07-08 01:30:36 +0300

    Add New Article

---
+++ Aws-F-F/Controllers/ArticlesController.cs
@@ -0,0 +1,85 @@
+﻿using Aws_F_F.Data;
+using Aws_F_F.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Aws_F_F.Controllers
+{
+    public class ArticlesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ArticlesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var articles = await _context.Articles.ToListAsync();
+            return View(articles);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Article article, IFormFile? imageFile)
+        {
+            if (ModelState.IsValid)
+            {
+                if (imageFile != null && imageFile.Length > 0)
+                {
+                    var uploadsFolder = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/uploads");
+                    Directory.CreateDirectory(uploadsFolder); // يتأكد إن المجلد موجود
+
+                    var uniqueFileName = Guid.NewGuid().ToString() + Path.GetExtension(imageFile.FileName);
+                    var filePath = Path.Combine(uploadsFolder, uniqueFileName);
+
+                    using (var fileStream = new FileStream(filePath, FileMode.Create))
+                    {
+                        await imageFile.CopyToAsync(fileStream);
+                    }
+
+                    article.ImagePath = "/uploads/" + uniqueFileName;
+                }
+
+                article.PublishDate = DateTime.Now;
+                _context.Add(article);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(article);
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> UploadImage(IFormFile upload)  // الصور من المحتوي
+        {
+            if (upload != null && upload.Length > 0)
+            {
+                var uploadsFolder = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/uploads");
+                Directory.CreateDirectory(uploadsFolder);
+
+                var fileName = Guid.NewGuid().ToString() + Path.GetExtension(upload.FileName);
+                var filePath = Path.Combine(uploadsFolder, fileName);
+
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    await upload.CopyToAsync(stream);
+                }
+
+                var imageUrl = Url.Content($"~/uploads/{fileName}");
+                return Json(new { url = imageUrl });
+            }
+
+            return BadRequest();
+        }
+
+
+    }
+}
