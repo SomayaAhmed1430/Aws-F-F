18b8aff34ded5d6ff031bbaa47bbfb453262c0ea
SomayaAhmed1430 <s.ramadan02185@dmu.edu.eg>
2025-07-10 04:49:06 +0300

    Home Layout

--- Aws-F-F/Controllers/HomeController.cs
+++ Aws-F-F/Controllers/HomeController.cs
@@ -1,16 +1,21 @@
 using System.Diagnostics;
 using Aws_F_F.Models;
+using Aws_F_F.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Linq;
 
 namespace Aws_F_F.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
@@ -21,6 +26,28 @@ namespace Aws_F_F.Controllers
         public IActionResult Privacy()
         {
             return View();
+        }
+
+        public IActionResult Videos()
+        {
+            var videos = _context.Videos.OrderByDescending(v => v.UploadDate).ToList();
+            return View(videos);
+        }
+
+        public IActionResult Articles()
+        {
+            var articles = _context.Articles.OrderByDescending(a => a.PublishDate).ToList();
+            return View(articles);
+        }
+
+        public IActionResult ArticleDetails(int id)
+        {
+            var article = _context.Articles.FirstOrDefault(a => a.Id == id);
+            if (article == null)
+            {
+                return NotFound();
+            }
+            return View(article);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
